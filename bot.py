d06f4e499001178aeb5fc47481b4170f83749446
Anubhav <132827423+AnubhavXD@users.noreply.github.com>
2024-06-04 02:59:17 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -88,10 +88,10 @@ async def _start(event):
             f"**Enjoy Ongoing Anime's Best Encode 24/7 🫡**",
             buttons=[
                 [
-                    Button.url("👨‍💻 DEV", url="t.me/kaif_00z"),
+                    Button.url("Owner", url="t.me/LuffyIsOnline"),
                     Button.url(
-                        "💖 OPEN SOURCE",
-                        url="https://github.com/kaif-00z/AutoAnimeBot/",
+                        "Channel",
+                        url="t.me/AnimeHarbour",
                     ),
                 ]
             ],

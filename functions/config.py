ac46b637476c6da893c6cd5cbd04f8c05422d064
Anubhav <132827423+AnubhavXD@users.noreply.github.com>
2024-06-04 16:34:46 +0530

    Update config.py

--- functions/config.py
+++ functions/config.py
@@ -50,6 +50,6 @@ class Var:
     )
     FFMPEG = config("FFMPEG", default="ffmpeg")
     CRF = config("CRF", default="27")
-    SEND_SCHEDULE = config("SEND_SCHEDULE", default=False, cast=bool)
+    SEND_SCHEDULE = config("SEND_SCHEDULE", default=True, cast=bool)
     RESTART_EVERDAY = config("RESTART_EVERDAY", default=True, cast=bool)
     FORCESUB_CHANNEL_LINK = config("FORCESUB_CHANNEL_LINK", default="", cast=str)
